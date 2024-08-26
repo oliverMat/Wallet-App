b06970cca9b28724b510c3d09d80ae80f70b5735
Mateus Oliveira <mateus.silva@b2ml.com.br>
2024-08-26 13:22:42 -0300

    Update MoneyView.kt

--- app/src/main/java/com/oliver/wallet/ui/view/money/MoneyView.kt
+++ app/src/main/java/com/oliver/wallet/ui/view/money/MoneyView.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
@@ -13,6 +14,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -32,7 +34,9 @@ fun MoneyView(navController: NavHostController, viewModel: MoneyViewModel = view
     val price by viewModel.price.collectAsState()
     val moneyChart by viewModel.moneyChart.collectAsState()
 
-    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+    Column(modifier = Modifier
+        .verticalScroll(rememberScrollState())
+        .padding(top = 10.dp)) {
         Row(
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically,
